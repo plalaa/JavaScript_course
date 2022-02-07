b545a6c33c413154a1e4d01780ce0cda7caa7911
plalaa <peterlala@icloud.com>
2022-02-07 20:54:28 +0100

    Values & Variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,6 +1,25 @@
 let js = 'amazing';
-if (js === 'amazing') alert('JS is amazing!');
-
-40 + 8 + 23 - 10
 console.log(40 + 8 + 23 - 10);
-// test
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # JavaScript_course
  
+
+- 1 db egyenlőség jel ad értéket a változónak
+- változó nevek: számok, betűk, alulvonás, vagy dollár szimbólum
+- változó csak ezekkel kezdődhet: betű, dollár szimbólum, alulvonás
+- változó neve ne legyen nagy kezdőbetűs
+- változó neve csupa nagybetűs, ha konstanst deklarálunk (pl. pi)
